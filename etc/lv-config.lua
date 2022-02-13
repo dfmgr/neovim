5f24c1a95de0d835273820d1554d4c1341989af1
Jason <git-admin@casjaysdev.com>
2022-02-13 11:06:58 -0500

    🗃️ modified: etc/lv-config.lua 🗃️

--- etc/lv-config.lua
+++ etc/lv-config.lua
@@ -43,18 +43,3 @@ O.lang.python.analysis.use_library_code_types = true
 
 -- javascript
 O.lang.tsserver.linter = nil
-
--- Additional Plugins
--- O.user_plugins = {{"windwp/nvim-ts-autotag"}}
-
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- O.user_autocommands = {{ "BufWinEnter", "*", "echo \"hi again\""}}
-
--- Additional Leader bindings for WhichKey
--- O.user_which_key = {
---   A = {
---     name = "+Custom Leader Keys",
---     a = { "<cmd>echo 'first custom command'<cr>", "Description for a" },
---     b = { "<cmd>echo 'second custom command'<cr>", "Description for b" },
---   },
--- }
